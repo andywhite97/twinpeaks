867656f1e9b1feed535d9c7553e6f062b2b0d2b8
Andy <andycreativestudios@gmail.com>
2026-08-09 22:56:21 +0200

    Auto-generated commit

---
+++ chunk-EZEM3BWT.js
@@ -0,0 +1,1 @@
+var n={production:!0,apiUrl:"https://twinpeaks-back.onrender.com/api",gaId:"G-ZSRC2TBKZX"};export{n as a};
--- chunk-FG6P5G2Z.js
+++
@@ -1,1 +0,0 @@
-var t={production:!0,apiUrl:"http://127.0.0.1:8000/api",gaId:"G-ZSRC2TBKZX"};export{t as a};
