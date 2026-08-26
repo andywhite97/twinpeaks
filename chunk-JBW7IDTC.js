4c261075ebca3004b22820f6fe216b82c3808b39
Andy <andycreativestudios@gmail.com>
2026-08-26 17:56:04 +0200

    Auto-generated commit

---
+++ chunk-JBW7IDTC.js
@@ -0,0 +1,1 @@
+import{a as i}from"./chunk-KXI3BQLE.js";import{l as r,o}from"./chunk-TL35FK5H.js";var c=class e{constructor(t){this.api=t}getProjects(){return this.api.get("projects/")}getProject(t){return this.api.get(`projects/${t}/`)}static \u0275fac=function(p){return new(p||e)(o(i))};static \u0275prov=r({token:e,factory:e.\u0275fac,providedIn:"root"})};export{c as a};
